e843c61b0ab5ed2ebc704db9ebbd53f9990edb6c
adko <gfadam97@gmail.com>
2020-04-28 01:49:29 +0200

    Minor changes

--- code/Development/org.mdse.pts.schedule.dsl/src/org/mdse/pts/schedule/dsl/linking/ScheduleLinkingService.java
+++ code/Development/org.mdse.pts.schedule.dsl/src/org/mdse/pts/schedule/dsl/linking/ScheduleLinkingService.java
@@ -7,13 +7,16 @@ import org.eclipse.emf.ecore.EReference;
 import org.eclipse.xtext.linking.impl.DefaultLinkingService;
 import org.eclipse.xtext.linking.impl.IllegalNodeException;
 import org.eclipse.xtext.nodemodel.INode;
+import org.mdse.pts.schedule.SchedulePackage;
 
 public class ScheduleLinkingService extends DefaultLinkingService {
 
 	@Override
 	public List<EObject> getLinkedObjects(EObject context, EReference reference, INode node) throws IllegalNodeException {
 		//TODO Implement custom linking service if needed.
+		//if(reference == SchedulePackage.eINSTANCE.getSchedule_WithDepot()) {
 		
+		//}
 		
 		return super.getLinkedObjects(context, reference, node);
 	}
