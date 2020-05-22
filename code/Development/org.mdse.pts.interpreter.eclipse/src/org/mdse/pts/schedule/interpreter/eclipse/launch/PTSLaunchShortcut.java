62126bac133ea58159d0b9e8e5fb97307b1afbd0
Jan Schill <janschill@me.com>
2020-05-22 18:12:11 +0200

    Generate files

--- code/Development/org.mdse.pts.interpreter.eclipse/src/org/mdse/pts/schedule/interpreter/eclipse/launch/PTSLaunchShortcut.java
+++ code/Development/org.mdse.pts.interpreter.eclipse/src/org/mdse/pts/schedule/interpreter/eclipse/launch/PTSLaunchShortcut.java
@@ -1,11 +1,14 @@
 package org.mdse.pts.schedule.interpreter.eclipse.launch;
 
-import java.io.OutputStream;
+import java.util.Collection;
+import java.util.List;
 
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
 import org.eclipse.debug.ui.ILaunchShortcut;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.edit.domain.EditingDomain;
 import org.eclipse.emf.edit.domain.IEditingDomainProvider;
 import org.eclipse.jface.dialogs.InputDialog;
@@ -13,93 +16,101 @@ import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.console.MessageConsole;
+import org.mdse.pts.common.util.EclipseUtil;
+import org.mdse.pts.common.util.EcoreIOUtil;
 import org.mdse.pts.schedule.Schedule;
 import org.mdse.pts.schedule.interpreter.ScheduleInterpreter;
 import org.mdse.pts.schedule.interpreter.eclipse.PTSEclipseUtil;
+import org.mdse.pts.timetable.Timetable;
 
 public class PTSLaunchShortcut implements ILaunchShortcut {
 
 	@Override
 	public void launch(ISelection selection, String mode) {
-		//Load model
-		IFile scheduelFile = PTSEclipseUtil.getFirstIFileFromSelectionWithExtension("schedule", selection);
-		Schedule scheduleModel = (Schedule) PTSEclipseUtil.loadModel(scheduelFile);
+		IFile scheduleFile = PTSEclipseUtil.getFirstIFileFromSelectionWithExtension("schedule", selection);
+		EObject eObject = PTSEclipseUtil.loadModel(scheduleFile);
+		ResourceSet rs = EcoreIOUtil.getDefaultResourceSet(eObject);
+		IProject project = EclipseUtil.resolveProjectFromResource(rs.getResources().get(0));
+		Schedule scheduleModel = (Schedule) eObject;
+		Collection<Timetable> timetables = interpretScheduleModel(scheduleModel);
 
-		interpretScheduleModel(scheduleModel);
+		for (Timetable timetable : timetables) {
+			IFile file = project.getFile(removeWhitespace(timetable.getStationName()) + ".timetable");
+			EcoreIOUtil.saveModelAs(scheduleModel, file);
+		}
 	}
-	
+
+	private static String removeWhitespace(String stringValue) {
+		return stringValue.replace(' ', '-').toLowerCase();
+	}
+
 	@Override
 	public void launch(IEditorPart editor, String mode) {
-		EObject model = getModelFromEditor(editor);
-		
-		if (model instanceof Schedule) {
-			Schedule scheduleModel = (Schedule) model;
-			interpretScheduleModel(scheduleModel);
+		EObject eObject = getModelFromEditor(editor);
+		ResourceSet rs = EcoreIOUtil.getDefaultResourceSet(eObject);
+		IProject project = EclipseUtil.resolveProjectFromResource(rs.getResources().get(0));
+
+		if (eObject instanceof Schedule) {
+			Schedule scheduleModel = (Schedule) eObject;
+			Collection<Timetable> timetables = interpretScheduleModel(scheduleModel);
+			for (Timetable timetable : timetables) {
+				IFile file = project.getFile(removeWhitespace(timetable.getStationName()) + ".timetable");
+				EcoreIOUtil.saveModelAs(scheduleModel, file);
+			}
+
 			return;
 		}
-		
+
 		String title = "Error";
 		String message = "The file cannot be interpreted as a Schedule model.";
 		MessageDialog.openError(null, title, message);
 	}
-	
+
 	protected EObject getModelFromEditor(IEditorPart editor) {
-		//The generated Ecore editor implements this interface.
 		if (editor instanceof IEditingDomainProvider) {
 			IEditingDomainProvider editingDomainProvider = (IEditingDomainProvider) editor;
 			EditingDomain editingDomain = editingDomainProvider.getEditingDomain();
-			
-			//This is how the generated Ecore editor accesses the resource internally.
-			//Still seems brittle esp. when there is another model in the editing domain.
+
 			Resource resource = editingDomain.getResourceSet().getResources().get(0);
 			EObject model = resource.getContents().get(0);
-			
+
 			return model;
 		}
-		
+
 		return null;
 	}
-	
-	protected void interpretScheduleModel(Schedule scheduleModel) {
-		//Ask for input
-		String interpreterInputPath = askForInterpreterPath();
-		
-		if (interpreterInputPath != null) {
-			//Interpret FSM
-			interpretScheduleModel(scheduleModel, interpreterInputPath);
-		}
-	}
-	
+//	
+//	protected void interpretScheduleModel(Schedule scheduleModel) {
+//		String interpreterInputPath = askForInterpreterPath();
+//		
+//		if (interpreterInputPath != null) {
+//			interpretScheduleModel(scheduleModel, interpreterInputPath);
+//		}
+//	}
+
 	protected String askForInterpreterPath() {
-		InputDialog inputDialog = new InputDialog(null, "Schedule Interpreter Input", "Please provide the path to your Schedule path", null, null);
-		
+		InputDialog inputDialog = new InputDialog(null, "Schedule Interpreter Input",
+				"Please provide the path to your Schedule path", null, null);
+
 		int returnCode = inputDialog.open();
-		
+
 		if (returnCode == InputDialog.OK) {
 			return inputDialog.getValue();
 		}
-		
+
 		return null;
 	}
 
-	protected void interpretScheduleModel(Schedule scheduleModel, String interpreterInput) {
+	protected Collection<Timetable> interpretScheduleModel(Schedule scheduleModel) {
 		ScheduleInterpreter interpreter = new ScheduleInterpreter();
-		
-		try {
-			//Reroute output to console of runtime instance
-			MessageConsole console = PTSEclipseUtil.findOrCreateConsole("FSM Interpreter");
-			console.clearConsole();
-			console.activate();
-//			TODO: Simulate train
-//			TODO: Create new timetable model
-			OutputStream consoleOutputStream = console.newOutputStream();
-			
-//			interpreter.interpret(model, interpreterInput, consoleOutputStream);
-		} catch(Exception e) {
-			String title = "Error";
-			String message = e.getMessage();
-			MessageDialog.openError(null, title, message);
-		}
+
+		MessageConsole console = PTSEclipseUtil.findOrCreateConsole("Schedule Interpreter");
+		console.clearConsole();
+		console.activate();
+		Collection<Timetable> timetables = interpreter.interpret(scheduleModel);
+
+		return timetables;
+
 	}
-	
+
 }
--- code/Development/org.mdse.pts.interpreter/src/org/mdse/pts/schedule/interpreter/ScheduleInterpreter.java
+++ code/Development/org.mdse.pts.interpreter/src/org/mdse/pts/schedule/interpreter/ScheduleInterpreter.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collection;
 import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.List;
@@ -14,49 +15,23 @@ import org.mdse.pts.depot.*;
 import org.mdse.pts.network.*;
 import org.mdse.pts.schedule.*;
 import org.mdse.pts.timetable.*;
-import org.mdse.pts.time.*;
 import org.mdse.pts.time.Time;
 import org.mdse.pts.time.Day;
 import org.mdse.pts.timetable.impl.TimetableFactoryImpl;
-import org.mdse.pts.timetable.impl.TimetableImpl;
 import org.mdse.pts.time.impl.TimeFactoryImpl;
 
 public class ScheduleInterpreter {
-	public List<Timetable> interpret(Schedule schedule) {
+	public Collection<Timetable> interpret(Schedule schedule) {
 		HashMap<String, Timetable> simulatedTrains = simulateTrains(schedule);
-		List<Timetable> timetables = generateTimetables(schedule, simulatedTrains);
-		return timetables;
+		return simulatedTrains.values();
 	}
 
-//	TODO: I am not sure about this
-	private static List<Timetable> generateTimetables(Schedule schedule, HashMap<String, Timetable> simulatedTrains) {
-		Network network = (Network) schedule.getNetworkReference().getNetwork();
-		List<Timetable> timetables = new ArrayList<Timetable>();
-		EList<Station> stations = network.getStations();
-
-		for (Station station : stations) {
-			Timetable timetable = new TimetableFactoryImpl().createTimetable();
-			timetable.setStationName(station.getName());
-//			Timetable stationTimetable = simulatedTrains.get(station.getName());
-//			addArrivalsToTimetableFromStationTimetable(station, timetable, stationTimetable.arrivals);
-//			addDepartureToTimetableFromStationTimetable(station, timetable, stationTimetable.departures);
-			timetables.add(timetable);
-		}
-
-		return timetables;
-	}
-
-//	TODO: This is getting somewhere, but I am unsure about this approach
-//	on generating all the correct timetables
-//	Station startStation = route.getTransits().get(0).getStation();
-//	It communicates the idea.
 	private static HashMap<String, Timetable> simulateTrains(Schedule schedule) {
 		HashMap<String, Timetable> trips = new HashMap<String, Timetable>();
 		EList<Route> routes = schedule.getRouteReference(); // It is how it is.
 
 		for (Route route : routes) {
 			Train currentTrain = route.getTrain();
-			int currentTravelTime = 0;
 			List<STime> startingTimes = route.getTime();
 			Transit previousTransit = null;
 			int index = 1;
@@ -65,6 +40,7 @@ public class ScheduleInterpreter {
 				Timetable timetable = trips.get(currentStation.getName());
 				if (timetable == null) {
 					timetable = new TimetableFactoryImpl().createTimetable();
+					timetable.setStationName(currentStation.getName());
 				}
 				Transit nextTransit = null;
 				if (index < route.getTransits().size()) {
@@ -72,25 +48,19 @@ public class ScheduleInterpreter {
 				}
 				Leg leg = transit.getLeg();
 				if (previousTransit != null) {
-					int travelTimeInMinutes;
-					if (leg == null) {
-						travelTimeInMinutes = getTravelTimeInMinutes(previousTransit.getStation(), currentStation, currentTrain);
-					} else {
-						travelTimeInMinutes = getTravelTimeInMinutes(leg, currentTrain);
-					}
-					
-					List<Departure> previousTransitDepartures = trips.get(previousTransit.getStation().getName()).getEntries().stream()
-							.filter(Departure.class::isInstance)
-							.map(Departure.class::cast)
+					int travelTimeInMinutes = getTravelTimeInMinutes(leg, previousTransit, currentStation, currentTrain);
+
+					List<Departure> previousTransitDepartures = trips.get(previousTransit.getStation().getName())
+							.getEntries().stream().filter(Departure.class::isInstance).map(Departure.class::cast)
 							.filter(d -> d.getToStation().equals(currentStation.getName()))
-							.filter(t -> t.getTrain().equals(currentTrain.getName()))
-							.collect(Collectors.toList());
+							.filter(t -> t.getTrain().equals(currentTrain.getName())).collect(Collectors.toList());
 					for (Departure previousDeparture : previousTransitDepartures) {
 						Arrival arrival = new TimetableFactoryImpl().createArrival();
 						arrival.setTrain(currentTrain.getName());
 						arrival.setFromStation(previousTransit.getStation().getName());
 						arrival.setPlatform(transit.getPlatform().getName());
-						arrival.setTime(calculateTravelTime(previousDeparture.getTime(), travelTimeInMinutes));
+						Time arrivalTime = addTime(previousDeparture.getTime(), travelTimeInMinutes);
+						arrival.setTime(arrivalTime);
 						timetable.getEntries().add(arrival);
 
 						if (nextTransit != null) {
@@ -98,21 +68,25 @@ public class ScheduleInterpreter {
 							departure.setTrain(currentTrain.getName());
 							departure.setToStation(nextTransit.getStation().getName());
 							departure.setPlatform(transit.getPlatform().getName());
-//							TODO: if standingDuration null time is 0
-//							departure.setTime(setDepartureTime, currentTravelTime, travelTime, transit.getStandingDuration());
+//							TODO: could be null, probably not
+							departure.setTime(addTime(arrivalTime, transit.getStandingDuration()));
 							timetable.getEntries().add(departure);
 						}
 					}
 				} else {
 					if (nextTransit != null) {
 						for (STime time : startingTimes) {
-							for (time.Day day : time.getDay()) {
+							for (Day day : time.getDay()) {
 								for (HrMin hrMin : time.getHrmin()) {
 									Departure departure = new TimetableFactoryImpl().createDeparture();
 									departure.setTrain(currentTrain.getName());
 									departure.setToStation(nextTransit.getStation().getName());
 									departure.setPlatform(nextTransit.getPlatform().getName());
-//									departure.setTime(setDepartureTime, currentTravelTime, travelTime, transit.getStandingDuration());
+									Time departureTime = new TimeFactoryImpl().createTime();
+									departureTime.setDay(day);
+									departureTime.setHour(hrMin.getHour());
+									departureTime.setMinute(hrMin.getMinute());
+									departure.setTime(departureTime);
 									timetable.getEntries().add(departure);
 								}
 							}
@@ -128,23 +102,34 @@ public class ScheduleInterpreter {
 		return null;
 	}
 
-	private static Time calculateTravelTime(Time previousTime, int travelTimeInMinutes) {
+	private static int getTravelTimeInMinutes(Leg leg, Transit previousTransit, Station currentStation, Train currentTrain) {
+		if (leg == null) {
+			return getTravelTimeInMinutes(previousTransit.getStation(), currentStation,
+					currentTrain);
+		} else {
+			return getTravelTimeInMinutes(leg, currentTrain);
+		}
+	}
+	
+	private static Time addTime(Time previousTime, int travelTimeInMinutes) {
 		Day day = previousTime.getDay();
 		int hour = previousTime.getHour();
 		int minute = previousTime.getMinute();
-		
+
 		Calendar cal = GregorianCalendar.getInstance();
 		cal.set(Calendar.DAY_OF_WEEK, day.getValue());
 		cal.set(Calendar.HOUR_OF_DAY, hour);
 		cal.set(Calendar.MINUTE, minute);
 		cal.add(Calendar.MINUTE, travelTimeInMinutes);
-		
+
 		Time travelTime = new TimeFactoryImpl().createTime();
 		travelTime.setDay(Day.get(cal.get(Calendar.DAY_OF_WEEK)));
+		travelTime.setHour(cal.get(Calendar.HOUR_OF_DAY));
+		travelTime.setMinute(cal.get(Calendar.MINUTE));
 
 		return travelTime;
 	}
-	
+
 	private static Leg getLegBetweenStations(Station previousStation, Station station) {
 		if (previousStation != null) {
 			for (Leg leg : previousStation.getLegs()) {
@@ -154,14 +139,6 @@ public class ScheduleInterpreter {
 			}
 		}
 		return null;
-	}
-
-	private static void castTime(STime stime) {
-		Time time = new TimeFactoryImpl().createTime();
-		int dayValue = stime.getDay().get(0).getValue();
-		time.setDay(Day.get(dayValue));
-//		time.setHour(stime);
-//		time.setMinute(minute);
 	}
 
 	private static int getTravelTimeInMinutes(Leg leg, Train train) {
@@ -174,13 +151,6 @@ public class ScheduleInterpreter {
 		return getTravelTimeInMinutes(leg, train);
 	}
 
-//
-//	private static time.Time calculateTravelTime() {
-//		time.Time travelTime = (time.Time) new TimeFactoryImpl().createTime();
-//
-//		return travelTime;
-//	}
-	
 	private static int getAverageSpeed(Train train) {
 		if (train instanceof RegionalTrain) {
 			return 80;
@@ -190,28 +160,6 @@ public class ScheduleInterpreter {
 			} else {
 				return 150;
 			}
-		}
-	}
-
-	private static void addArrivalsToTimetableFromStationTimetable(Station station, Timetable timetable,
-			List<Arrival> arrivals) {
-		for (Arrival arrival : arrivals) {
-			Arrival entry = new TimetableFactoryImpl().createArrival();
-			entry.setTrain(arrival.getTrain());
-			entry.setTime(arrival.getTime());
-			entry.setFromStation(station.getName());
-			timetable.getEntries().add(entry);
-		}
-	}
-
-	private static void addDepartureToTimetableFromStationTimetable(Station station, Timetable timetable,
-			List<Departure> departures) {
-		for (Departure departure : departures) {
-			Departure entry = new TimetableFactoryImpl().createDeparture();
-			entry.setTrain(departure.getTrain());
-			entry.setTime(departure.getTime());
-			entry.setToStation(station.getName());
-			timetable.getEntries().add(entry);
 		}
 	}
 }
